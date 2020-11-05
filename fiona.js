5163fa535f0307f367d7b8726ced09880193cce6
Preetam D'Souza <preetamjdsouza@gmail.com>
2020-11-04 20:57:10 -0500

    init

---
+++ fiona.js
@@ -0,0 +1,154 @@
+// Functional Programming support
+
+// -- core -----------------------------------------------------------
+
+const partial = (fn, ...args) => fn.bind(null, ...args);
+
+// curry :: ((a, b, ...) -> c) -> a -> b -> ... -> c
+// from https://mostly-adequate.gitbooks.io/mostly-adequate-guide/appendix_a.html#curry
+const curry = (fn) => {
+  return function _curry(...args) {
+    if (args.length < fn.length) {
+      return partial(_curry, ...args);
+    }
+    return fn.call(null, ...args);
+  };
+};
+
+const pipe = (...fns) => curry((x) => fns.reduce((acc, fn) => fn(acc), x));
+
+const comp = (...fns) => pipe(...fns.reverse());
+
+// -- pointfree ------------------------------------------------------
+
+// identity :: a -> a
+const identity = (a) => a;
+
+// constant :: c -> b -> c
+const constant = curry((c, x) => c);
+
+// map :: Functor f => (a -> b) -> f a -> f b
+const map = curry((fn, f) => f.map(fn));
+
+// reduce :: ((a, b) -> a) -> [b] -> a
+const reduce = curry((fn, f) => f.reduce(fn));
+
+// eq :: a -> b -> Boolean
+const eq = curry((a, b) => a === b);
+
+// not :: Boolean -> Boolean
+const not = (x) => !x;
+
+// trace! :: a -> a
+const trace = (x) => {
+  console.debug(x);
+  return x;
+};
+
+// omit :: prop -> Object -> Object
+const omit = (prop, obj) =>
+  Object.keys(obj)
+    .filter(comp(not, eq(prop)))
+    .reduce((acc, k) => {
+      acc[k] = obj[k];
+      return acc;
+    }, {});
+
+// some :: (a -> Boolean) -> [a] -> Boolean
+// Returns true if some x in coll satisfies pred.
+const some = (pred, coll) => {
+  if (!coll) {
+    return false;
+  }
+
+  for (let x of coll) {
+    if (pred(x)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// every :: (a -> Boolean) -> [a] -> Boolean
+// Returns true if all x in coll satisfies pred.
+const every = (pred, coll) => {
+  if (!coll) {
+    return true;
+  }
+
+  for (let x of coll) {
+    if (!pred(x)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Returns the first n elements from coll.
+// Returns all the elements of coll if coll.length < n
+const take = (n, coll) => coll.slice(0, n);
+
+// Returns coll with the first n items removed.
+const drop = (n, coll) => coll.slice(n);
+
+// Partitions a sequence into a sequence of lists
+// of size n.
+// The final sequence may be <= n if coll.length % n != 0.
+const partition = (n, coll) => {
+  const p = [];
+  while (coll.length) {
+    p.push(take(n, coll));
+    coll = drop(n, coll);
+  }
+  return p;
+};
+
+// Returns a sequence [0, n).
+const range = (n) =>
+  Array(n)
+    .fill()
+    .map((_, i) => i);
+
+// Concatenate two strings.
+// cat :: String -> String -> String
+const cat = (a, b) => a + b;
+
+// Split a string based on a separator.
+// split :: String -> String -> String
+const split = (sep, s) => s.split(sep);
+
+// Append an item to a list.
+// append :: a -> [a] -> [a]
+const append = curry((x, xs) => xs.concat([x]));
+
+// Return the nth item in an array.
+// nth :: n -> [a] -> a
+const nth = curry((n, xs) => (xs ? xs[n] : null));
+
+// Return the first item in an array.
+// first :: [a] -> a
+const first = nth(0);
+
+// Return the second item in an array.
+// second :: [a] -> a
+const second = nth(1);
+
+export {
+  curry,
+  pipe,
+  comp,
+  identity,
+  map,
+  reduce,
+  not,
+  omit,
+  every,
+  some,
+  take,
+  drop,
+  partition,
+  range,
+  nth,
+  first,
+  second,
+};
---
+++ fiona.test.js
@@ -0,0 +1,81 @@
+import {
+  every,
+  some,
+  take,
+  drop,
+  partition,
+  range,
+  nth,
+  first,
+  second,
+} from "./fiona";
+
+test("every", () => {
+  const isOdd = (x) => x % 2 !== 0;
+  expect(every(isOdd, undefined)).toBe(true);
+  expect(every(isOdd, [])).toBe(true);
+  expect(every(isOdd, [1])).toBe(true);
+  expect(every(isOdd, [2])).toBe(false);
+  expect(every(isOdd, [1, 2, 3])).toBe(false);
+  expect(every(isOdd, [1, 3, 5, 7, 9, 11, 13, 132])).toBe(false);
+  expect(every(isOdd, [1, 3, 5, 7, 9, 11, 13, 131])).toBe(true);
+});
+
+test("some", () => {
+  const isOdd = (x) => x % 2 !== 0;
+  expect(some(isOdd, undefined)).toBe(false);
+  expect(some(isOdd, [])).toBe(false);
+  expect(some(isOdd, [1])).toBe(true);
+  expect(some(isOdd, [2])).toBe(false);
+  expect(some(isOdd, [1, 2, 3])).toBe(true);
+  expect(some(isOdd, [1, 3, 5, 7, 9, 11, 13, 132])).toBe(true);
+  expect(some(isOdd, [2, 4, 6, 8, 10, 12, 14, 28])).toBe(false);
+});
+
+test("take", () => {
+  expect(take(2, [0])).toEqual([0]);
+  expect(take(2, [])).toEqual([]);
+  expect(take(2, [0, 1, 2, 3, 4, 5])).toEqual([0, 1]);
+  expect(take(10, [0, 1, 2, 3, 4, 5])).toEqual([0, 1, 2, 3, 4, 5]);
+});
+
+test("drop", () => {
+  expect(drop(2, [0])).toEqual([]);
+  expect(drop(2, [])).toEqual([]);
+  expect(drop(2, [0, 1, 2, 3, 4, 5])).toEqual([2, 3, 4, 5]);
+});
+
+test("partition", () => {
+  expect(partition(2, [0, 1, 2, 3, 4, 5])).toEqual([
+    [0, 1],
+    [2, 3],
+    [4, 5],
+  ]);
+  expect(partition(2, [0, 1, 2, 3, 4])).toEqual([[0, 1], [2, 3], [4]]);
+});
+
+test("range", () => {
+  expect(range(0)).toEqual([]);
+  expect(range(1)).toEqual([0]);
+  expect(range(10)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+});
+
+test("nth", () => {
+  expect(nth(0, null)).toEqual(null);
+  expect(nth(0, [])).toEqual(undefined);
+  expect(nth(0, [1, 2, 3])).toEqual(1);
+  expect(nth(1, [1, 2, 3])).toEqual(2);
+  expect(nth(2, [1, 2, 3])).toEqual(3);
+});
+
+test("first", () => {
+  expect(first(null)).toEqual(null);
+  expect(first([])).toEqual(undefined);
+  expect(first([1, 2, 3])).toEqual(1);
+});
+
+test("second", () => {
+  expect(second(null)).toEqual(null);
+  expect(second([])).toEqual(undefined);
+  expect(second([1, 2, 3])).toEqual(2);
+});
